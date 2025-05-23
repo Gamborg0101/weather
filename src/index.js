e05bf00bd75b67113ab41629ebe2ba6e07b4b4ab
Casper Gamborg <Gamborgc@gmail.com>
2025-05-23 12:55:32 +0200

    mostly done - only need small adjustment to css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ require("./style.css");
 const API_KEY =
   "WFGRQQUH947TD839JRKPSHXNB"; /* THIS DATA SHOULD BE HIDDEN - but since it's a public API, it does not really matter*/
 const location = "aarhus";
-const fetchAPI = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`;
 
 /* Get input from user */
 async function getInfo() {
@@ -21,26 +20,45 @@ getInfo();
 
 /* Call API with userinput */
 async function getData(location) {
-  location = "aarhus";
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`;
   try {
     const reponse = await fetch(url);
     const dataJSON = await reponse.json();
-    const weatherTemperature = dataJSON.currentConditions.temp;
-    convertToCelcius(weatherTemperature);
+
+    const weatherTempToday = dataJSON.currentConditions.temp;
+    const weatherTempTomorrow = dataJSON.days[1].temp;
+    const todayRise = dataJSON.currentConditions.sunrise;
+    const todaySunset = dataJSON.currentConditions.sunset;
+
+    displayForUser(
+      weatherTempToday,
+      weatherTempTomorrow,
+      todayRise,
+      todaySunset
+    );
   } catch (error) {
     console.error(error);
   }
 }
 
 function convertToCelcius(temp) {
-  const convertedTemperatur = (temp - 32) * (5 / 9);
-  displayForUser(convertedTemperatur);
+  const convTemp = (temp - 32) * (5 / 9);
+  return convTemp.toFixed(2);
 }
 
-function displayForUser(temp) {
-  console.log(temp);
-  document.body.append(temp);
+function displayForUser(tempToday, tempTomorrow, sunrise, sunset) {
+  const convTempToday = convertToCelcius(tempToday);
+  const convTempTomorrow = convertToCelcius(tempTomorrow);
+
+  const dataCurrent = document.getElementById("weather-data-current");
+  const dataTomorrow = document.getElementById("weather-data-tomorrow");
+  const dataSunrise = document.getElementById("weather-data-sunrise");
+  const dataSunset = document.getElementById("weather-data-sunset");
+
+  dataCurrent.append(` ${convTempToday} celcius`);
+  dataTomorrow.append(` ${convTempTomorrow} celcius`);
+  dataSunrise.append(`${sunrise}`);
+  dataSunset.append(`${sunset}`);
 }
 
-getData(fetchAPI);
+getData();
